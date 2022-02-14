4282401337eb4ffb587995a70d2f77798d40fd54
Joakim <wahlstrom.joakim@gmail.com>
2022-02-14 13:05:35 +0100

    lektion 3

---
+++ Lektion-2/3-todo/backend/controllers/todosController.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const todoModel = require('../models/todoModel');
+
+
+router.post('/', todoModel.saveTodo)
+
+router.get('/', todoModel.getTodos)
+
+router.patch('/:id', todoModel.updateTodo)
+
+router.delete('/:id', todoModel.deleteTodo)
+
+
+
+module.exports = router;
