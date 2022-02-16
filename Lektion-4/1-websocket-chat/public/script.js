25b4ac06d158436ed62987aed97e12299e37a6b3
Joakim <wahlstrom.joakim@gmail.com>
2022-02-16 15:12:03 +0100

    lektion 4

--- Lektion-4/1-websocket-chat/public/script.js
+++ Lektion-4/1-websocket-chat/public/script.js
@@ -3,6 +3,8 @@ const socket = io();
 const messages = document.querySelector('.messages');
 const chatForm = document.querySelector('#chatForm');
 const chatMessage = document.querySelector('#chatMessage');
+const chatWindow = document.querySelector('.chat-window');
+const feedback = document.querySelector('#feedback');
 
 const userName = new URLSearchParams(window.location.search).get('name');
 document.querySelector('#me').innerText = userName;
@@ -13,6 +15,36 @@ socket.on('connect', () => {
 
 socket.on('user', data => {
   messages.innerHTML += `<p class="inline-feedback">${data}</p>`
+  chatWindow.scrollTop = chatWindow.scrollHeight;
+})
+
+
+socket.on('message', data => {
+  let position = data.id === socket.id ? 'right' : ''
+
+  // feedback.insertAdjacentHTML('beforebegin', `
+  //   <div class="single-message ${position}">
+  //     <p class="single-message_name">${data.name}</p>
+  //     <p class="single-message_msg">${data.message}</p>
+  //   </div>
+  // `)
+  messages.innerHTML += `
+  <div class="single-message ${position}">
+    <p class="single-message_name">${data.name}</p>
+    <p class="single-message_msg">${data.message}</p>
+  </div>
+  `
+
+  feedback.innerText = ''
+  feedback.classList.add('d-none')
+
+  // console.log(chatWindow.scrollHeight);
+  chatWindow.scrollTop = chatWindow.scrollHeight;
+})
+
+socket.on('typing', data => {
+  feedback.innerText = `${data} is typing a message...`
+  feedback.classList.remove('d-none')
 })
 
 
@@ -30,4 +62,8 @@ chatForm.addEventListener('submit', e => {
   chatMessage.value = '';
   chatMessage.focus();
 
+})
+
+chatMessage.addEventListener('keypress', () => {
+  socket.emit('typing', userName)
 })
