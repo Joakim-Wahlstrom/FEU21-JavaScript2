c393456831d236ffbfa80df26017785e36128714
Joakim <wahlstrom.joakim@gmail.com>
2022-02-16 11:55:51 +0100

    lektion 4

--- Lektion-4/1-websocket-chat/index.js
+++ Lektion-4/1-websocket-chat/index.js
@@ -11,3 +11,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log('Server running on http://localhost:' + PORT));
+
+
+const io = socket(server)
+
+io.on('connection', socket => {
+  // console.log('New user connected ', socket.id)
+
+  socket.emit('message', 'Weclome to the chat!')
+
+})
