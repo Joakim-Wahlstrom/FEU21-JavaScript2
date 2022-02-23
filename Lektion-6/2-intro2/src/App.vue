a3cf53e6c84a8524e46c623a0a8665978cc4943f
Joakim <wahlstrom.joakim@gmail.com>
2022-02-23 11:51:22 +0100

    lektion 6

--- Lektion-6/2-intro2/src/App.vue
+++ Lektion-6/2-intro2/src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>asd</div>
-  <div>asdsa</div>
+  <Navbar />
+  <Carousel />
+  <Tesimonials />
+  <Footer />
+
 </template>
 
 <script>
+import Navbar from './components/navigation/Navbar.vue'
+import Footer from './components/navigation/Footer.vue'
+import Carousel from './components/Carousel.vue'
+import Tesimonials from './components/Testimonials/Tesimonials.vue'
 export default {
   name: 'App',
   components: {
-
+    Navbar,
+    // appFooter: Footer
+    Footer,
+    Carousel,
+    Tesimonials
   }
 }
 </script>
