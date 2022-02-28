b6c5d71df61b0f974bf2d34628347fbad9e98eaf
Joakim <wahlstrom.joakim@gmail.com>
2022-02-28 13:56:17 +0100

    lektion 7

---
+++ Lektion-7/3-conditionals/src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todos">
+    <div v-if="todos.length">
+      <p>{{todos[0].title}}</p>
+    </div>
+    <h2 v-else>Det finns inga todos, lägg till några vetja!</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'appTodos',
+  props: ['todos']
+}
+</script>
+
+<style scoped>
+  .todos {
+    margin-top: 2rem;
+  }
+</style>
